66c618fc3539dad29f9b32d47d7b8d339d6fef61
voxilous <tech132001@gmail.com>
2023-04-26 22:29:44 +0100

    added rendering with crossterm

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,56 @@
 use std::io;
-use std::io::BufWriter;
-use std::io::Write;
+pub use crossterm::{
+    cursor,
+    event::{self, Event, KeyCode, KeyEvent},
+    execute, queue, style,
+    terminal::{self, ClearType},
+    Command,
+};
 
-fn main() {
-    let mut board:[usize;100] = [0;100];
-    board[44] = 1;
-    render(&board);
+const COL:usize = 10;
+const ROW:usize = 10;
+const MAX :usize = COL * ROW;
+
+fn main() -> io::Result<()> {
+    //let mut board:[usize;MAX] = [0;MAX];
+    let mut visual:[char;MAX] = ['□';MAX];
+    
+    //let cursor = 44;
+    
+    let mut writer = set_up_terminal();    
+    draw(&mut writer,&visual)?;
+    io::Result::Ok(())
 }
 
-fn render(board:&[usize;100]){
-    let mut writer = BufWriter::new(io::stdout());
-    let characters = ["□", "■"];
-    for i in 0..board.len(){
-        let _result = writer.write(characters[board[i]].as_bytes());
-        if (i % 10) == 9{
-            let _result = writer.write("\n".as_bytes());
-        }
+fn set_up_terminal()-> io::Stdout
+{
+    let mut writer = io::stdout();
+    execute!(writer, terminal::EnterAlternateScreen).unwrap();
+    terminal::enable_raw_mode().unwrap();
+    writer
+}
+//fn input(){}
+//fn update(){}
+//fn render(){}
+fn draw<W>(w:&mut W,visual:&[char;MAX]) -> io::Result<()>
+where W : io::Write
+{
+    queue!(
+        w,
+        style::ResetColor,
+        terminal::Clear(terminal::ClearType::All),
+        cursor::Hide,
+        cursor::MoveTo(0,0)
+    )?;
+    for i in 0..ROW{
+        let line = &visual[i*COL..(i+1)*COL];
+        let string :String = line.iter().cloned().collect();
+        queue!(
+            w,
+            style::Print(string),
+            cursor::MoveToNextLine(1)
+        )?;
     }
-    let _size = writer.flush();
+    w.flush()?;
+    io::Result::Ok(())
 }
